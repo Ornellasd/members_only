b09cdcc41280fa58880a12d7956c2fb15ef9e164
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2020-11-13 20:40:56 -0800

    adds membership_status to user model

--- models/user.js
+++ models/user.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
-		username: {type: String, required: true, maxlength: 100},
+    username: {type: String, required: true, maxlength: 100},
     first_name: {type: String, required: true, maxlength: 100},
     last_name: {type: String, required: true, maxlength: 100},
     password: {type: String, required: true, maxlength: 100},
-    //membership_status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
+    membership_status: {type: String, required: true, enum: ['Admin', 'Normal', 'V.P.'], default: 'Normal'},
   }
 );
 
