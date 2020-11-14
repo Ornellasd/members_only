588662073ee057e28cdf471169407a8c3194b24b
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2020-11-13 18:51:15 -0800

    adds sanitization/validation to sign-up form

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,19 +1,20 @@
 const User = require('../models/user');
 const Message = require('../models/message');
+const { body,validationResult } = require('express-validator');
 
 exports.new_message_get = (req, res, next) => {
   res.render('new_message', { title: 'New Message'} );
 }
 
 exports.new_message_post = (req, res, next) => {
-	const message = new Message({
-		title: req.body.message_title,
-		text: req.body.message_text,
-		user: req.user._id,
-	}).save(err => {
-		if(err) {
-			return next(err);
-		};
-		res.redirect('/');
-	}) 	
+  const message = new Message({
+    title: req.body.message_title,
+    text: req.body.message_text,
+    user: req.user._id,
+  }).save(err => {
+    if(err) {
+      return next(err);
+    };
+    res.redirect('/');
+  })  
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,13 @@ const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const Message = require('../models/message');
-
+const { body,validationResult } = require('express-validator');
 
 exports.index = (req, res, next) => {
-
   Message.find({}, 'title timestamp text user')
     .populate('user')
     .exec((err, list_messages) => {
+      console.log(list_messages);
       res.render('index', {
         title: 'Member Only',
         user: req.user,
@@ -18,24 +18,53 @@ exports.index = (req, res, next) => {
 }
 
 exports.sign_up_get = (req, res, next) => {
-  res.render('sign-up', { title: 'Sign Up'});
+  res.render('sign-up', { title: 'Sign Up', errors: ''});
 }
 
-exports.sign_up_post = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    if(err) {
-      return next(err);
-    };
-    const user = new User({
-      username: req.body.username,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      password: hashedPassword
-    }).save(err => {
-      if(err) {
-        return next(err);
-      };
-      res.redirect('/');
-    })
-  });
-}
+exports.sign_up_post = [
+  body('username', 'username required').trim().isLength({
+    min: 1
+  }).escape(),
+  body('first_name', 'first name required').trim().isLength({
+    min: 1
+  }).escape(),
+  body('last_name', 'last name required').trim().isLength({
+    min: 1
+  }).escape(),
+  body('password', 'password required').trim().isLength({
+    min: 1
+  }).escape(),
+
+  (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.render('sign-up', {
+          title: 'Sign Up',
+          errors: errors.array()
+        });
+      return;
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if(err){
+          return next(err);
+        };
+
+        const user = new User({
+          username: req.body.username,
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          password: hashedPassword,
+        });
+
+        user.save((err) => {
+          if(err) {
+            return next(err);
+          };
+          res.redirect('/')
+        });
+      });
+    }
+  }
+];
