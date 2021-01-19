d5de699b289fd4a1fff9a895c116fa19018fd2bf
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-01-19 11:24:17 -0800

    format message timestamp and move modals to partials folder

--- models/message.js
+++ models/message.js
@@ -4,18 +4,16 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema(
   {
     title: {type: String, required: true, maxlength: 100},
-    timestamp: {type: Date, default: Date.now},
     text: {type: String, required: true},
     user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   }
 );
 
-//Virtual for message URL to delete
 MessageSchema
-.virtual('url')
+.virtual('formatted_timestamp')
 .get(function() {
-  return '/message/' + this._id;
+  const date = new Date();
+  return date.toLocaleDateString();
 });
 
-//Export model
 module.exports = mongoose.model('Message', MessageSchema);
